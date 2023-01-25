f849a323ee08a2fddaa33fff3c118cc714f2c3c3
Jasurbek <jasurdev1604@gmail.com>
2023-01-25 21:37:58 +0500

    about github

---
+++ github/index.js
@@ -0,0 +1,24 @@
+//repository -> project
+
+// connection
+// 1. git init 
+// 2. git remote add origin https://github.com/Jasurdev1604/none.git
+
+//con 
+//1. git config --global ures.email "jasurdev1604@gmail.com"
+//2. git config --global user.name "Jasurbek"
+
+//push
+//1. git add .
+//2. git commit -m 'test'
+//3. git push origin branchName 
+
+//pull
+//1. git pull origin branchName 
+
+//branch
+//1. git branch branchName -> create new branch
+//2. git checkout branchName -> yangi branchga o'tish
+//3. git branch -m branchName -> yangi branch yaratip unga otkazadi
+
+
