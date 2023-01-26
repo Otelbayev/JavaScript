061016639a74daee5b95b350f0426cc8caf5276a
Jasurbek <jasurdev1604@gmail.com>
2023-01-26 23:33:00 +0500

    Type Convetrion

--- 2-dars/test.js
+++ 2-dars/test.js
@@ -1,1 +1,30 @@
-console.log("test");
+//Type Convertion
+let str1 = "5"
+let num1 = 2
+let str2 = "Jasurbek"
+// console.log(+srt1); 1 type Number
+// console.log(+str2);  Nan
+// console.log(str1 + num1); 52
+// console.log(num1 + str1); 25
+// console.log(num1 * str1); 10
+// console.log(str2 * num1); NaN
+
+// Convert to Number
+
+let num2 = "10";
+let num3 = "10.5"
+/*
+console.log(Number(num2) , Number(num3)); 
+console.log(Number.parseInt(num2) , Number.parseInt(num3));
+console.log(Number.parseFloat(num2) , Number.parseFloat(num3));
+console.log(parseInt(num2) , parseInt(num3));
+console.log(parseFloat(num2) , parseFloat(num3));
+console.log(+true);
+console.log(+false);*/
+
+// Boolean
+/*
+console.log(true == "1"); //true
+console.log(true == "a"); //false
+console.log(true == "2"); //false
+console.log(true == "0"); //false*/
