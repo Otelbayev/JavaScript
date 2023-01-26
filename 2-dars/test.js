e34621315edacb8e0d8971daa59103ed48e8dc49
Jasurbek <jasurdev1604@gmail.com>
2023-01-27 00:31:41 +0500

    Type Convertion

--- 2-dars/test.js
+++ 2-dars/test.js
@@ -1,7 +1,7 @@
-//Type Convertion
-let str1 = "5"
-let num1 = 2
-let str2 = "Jasurbek"
+//!Type Convertion
+// let str1 = "5"
+// let num1 = 2
+// let str2 = "Jasurbek"
 // console.log(+srt1); 1 type Number
 // console.log(+str2);  Nan
 // console.log(str1 + num1); 52
@@ -9,7 +9,7 @@ let str2 = "Jasurbek"
 // console.log(num1 * str1); 10
 // console.log(str2 * num1); NaN
 
-// Convert to Number
+// !Convert to Number
 
 let num2 = "10";
 let num3 = "10.5"
@@ -22,9 +22,95 @@ console.log(parseFloat(num2) , parseFloat(num3));
 console.log(+true);
 console.log(+false);*/
 
-// Boolean
+//! Boolean
 /*
 console.log(true == "1"); //true
 console.log(true == "a"); //false
 console.log(true == "2"); //false
 console.log(true == "0"); //false*/
+
+//! Arufmetik amallar
+// + - * / % 
+// birinchi * keyin / keyin  + - 
+
+// console.log(111 % 10); // 1
+
+//! inkrement ++
+
+// let a = 10;
+// console.log(a++); //10
+// console.log(a); //11
+
+// let b = 20;
+// console.log(b); // 20
+// console.log(++b); //21
+
+//! dekriment --
+
+// let a = 10;
+// console.log(a--); //10
+// console.log(a); //9
+
+// let b = 20;
+// console.log(b); // 20
+// console.log(--b); //19
+
+//! Obrivations Qisqatriema inkriment va dekriment
+
+// let a = 50;
+
+// a = a + 10 ; // 60
+// a += 10; //70
+
+// console.log(a); //70
+
+//! Comparition - taqqolash
+
+// let a = 10 , b = 11 , c = 11;
+
+// console.log(a > b); // false
+// console.log(a < b); //true
+// console.log(b >= c); //true
+// console.log(b <= c); //true
+
+
+// let str1 = "a";
+// let str2 = "A";
+
+// console.log(str1 > str2); //true
+// console.log("str1" > "str2");//false
+// console.log("stur1" > "str2");//true
+// console.log("21" > "199"); //true
+
+// a = 1 , b = "1";
+
+// console.log(a == b); //true
+// console.log(a === b); //false
+
+// console.log(a != b); //false
+// console.log(a !== b); //true
+
+//! Logical opiration
+
+let a = true;
+let b = true;
+let c = false;
+
+//! OR - Yoki ||
+
+// console.log(a || b); //true
+// console.log(a || c); //true
+// console.log(false || c); // false
+
+//! AND &&
+
+// let id = true;
+// let face = true;
+// let none = false;
+
+// console.log(id && face);//true
+// console.log(id && none);//false
+// console.log(id && none && face);//false
+
+//! NOT !
+ // Inkor Hamma narsanng aksi
