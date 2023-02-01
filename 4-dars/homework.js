6b699e4bf3826babb3188bb7b836c9a6fe7d1a68
Jasurbek <jasurdev1604@gmail.com>
2023-02-01 22:00:54 +0500

    For loop While

---
+++ 4-dars/homework.js
@@ -0,0 +1,66 @@
+//!1
+/*
+let browser = "Safari";
+
+switch(browser) {
+    case 'Edge':
+        console.log(`You've got a ${browser}`);
+        break;
+    case 'Chrome':
+        console.log(`You've got a ${browser}`);
+        break;
+    case 'Safari':
+        console.log(`You've got a ${browser}`);
+        break;
+    case 'FireFox':
+        console.log(`You've got a ${browser}`);
+        break;
+    case 'Opera':
+        console.log(`You've got a ${browser}`);
+        break;
+    default:
+        console.log("We hope that this page looks ok!");
+}
+
+//!2
+
+let a = 1 // Barcha if larni tekshirib chiqadi consolega 1 ni chiqaradi
+a = 0 // consolega nol chiqadi
+a = 2 // consoleda 2,3 chiqadi
+a = 3 // consoleda 2,3 chiqadi
+
+if(a == 0) console.log(0);
+if(a == 1) console.log(1);
+if(a == 2 || a == 3) console.log('2,3');
+
+//!3
+
+let  i = 4 ;
+
+while(i) {
+    console.log(i--); //3 , 2 , 1 
+}  // 0 ga kelganda toxtaydi cadnition false boladi nol bolgan paytta
+
+//!4
+
+i = 0;
+while(++i < 5){
+    console.log(i); // 1 , 2 , 3 , 4
+}
+
+// o dan boshlamaganining sababi ++ inktement bor edi
+
+//!5
+
+i = 0;
+
+while(i++ < 5) {
+    console.log(i);
+} // 1,2,3,4,5
+
+// nol tekshirilganda consoleda bir chiqadi
+// bir tekshirilganda consoleda ikki chiqadi
+// va hokaza
+*/
+
+//!6
