ecab589a77703c4fd40f98819edd017ca9d3a2b7
Jasurbek <jasurdev1604@gmail.com>
2023-02-07 23:54:04 +0500

    objects

---
+++ 6-dars/homeWork.js
@@ -0,0 +1,80 @@
+//!1
+
+// Empty object
+let user = {};
+
+//Add name
+user.name = 'Jhon';
+//Add surname
+user.surname = 'Smith';
+//Change name
+user.name = 'Pete';
+//Delate name
+delete user.name;
+// console.log(user);
+
+//!2
+
+let salaries = {
+    Jhon: 100,
+    Ann:160,
+    Pete: 130 
+}
+let sum = 0;
+for (const key in salaries) {
+     sum += salaries[key];
+}
+// console.log(sum);
+
+//!3
+
+let menu = {
+    width:200,
+    height:300,
+    title:'My menu',
+}
+let menuClone = new Object();
+
+for (const key in menu) {
+    if(!isNaN(menu[key])){
+        menuClone[key] = menu[key] * 2 ;
+    } else {
+        menuClone[key] = menu[key];
+    }
+
+}
+// console.log(menuClone);
+
+//!4
+
+function makeUser() {
+    return {
+        name: 'Jhon',
+        ref:this
+    };
+}
+
+let userss = makeUser();
+
+// console.log(userss.ref.name); //undefined
+
+//!5
+
+let calculator = {
+    read(a , b){
+        this.a = a;
+        this.b = b;
+    },
+    sum(){
+        console.log(this.a + this.b);
+    },
+    mul(){
+        console.log(this.a * this.b);
+    }
+}
+
+// calculator.read(10 , 0);
+// calculator.sum();
+// calculator.mul();
+
+
