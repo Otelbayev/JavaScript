57165edec9d396f03f73447cd1d40d44567632f7
Jasurbek <jasurdev1604@gmail.com>
2023-02-08 17:10:31 +0500

    Object

--- 6-dars/homeWork.js
+++ 6-dars/homeWork.js
@@ -96,16 +96,18 @@ let ladder = {
 // ladder.up().up().down().showStep().down().showStep(); 
 
 //!7
-
+let obj = {
+    name:'Jasurbek'
+}
 function A() {
-    
+    return obj
 }
 function B() {
-    
+    return obj    
 }
 
 let a = new A();
 let b = new B();
 
-console.log(a);
+console.log(a == b);
 
