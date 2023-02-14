d98922cd3c37997ced85b3a2376d6bf476861e06
Jasurbek <jasurdev1604@gmail.com>
2023-02-15 00:04:32 +0500

    Array

---
+++ 9-dars/index.js
@@ -0,0 +1,21 @@
+//! Arrays
+
+//! Array declaration
+
+let array = [1 , 5 , "d" , '#' , 87540, "jasurbek" , null , undefined , function test() {}]
+
+// console.log(array);
+
+// console.log(array[0]);
+// console.log(array.length - 1); // oxitgi element
+// console.log(array.at(-5)); // oxiridan boshlab sanaydi
+
+//! new Array
+
+let arr = new Array()
+let arr1 = []
+
+arr1[100] = 1;
+
+console.log(arr1);
+console.log(arr1.length);
