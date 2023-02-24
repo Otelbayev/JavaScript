400bae540355b360c4e46a7be99aa72819720f89
Jasurbek <jasurdev1604@gmail.com>
2023-02-24 11:19:26 +0500

    Array

--- 9-dars/home.js
+++ 9-dars/home.js
@@ -25,3 +25,35 @@ let countChar = (array) => {
 // countChar(['webbrain' , 'academy' , '#$%^&*' , 'jasur%^&'])
 
 //!3
+
+let func = (ar1 , ar2) => {
+    if(ar1.length > ar2.length) console.log(`${ar1} array ${ar2} dan katta`);
+    else if(ar1.length < ar2.length) console.log(`${ar2} array ${ar1} dan katta`);
+    else {
+        let count = 0;
+        for(let i = 0; i < ar1.length; i++){
+            if(ar1[i] == ar2[i]) count++;
+            else count--;
+        }
+        if(count === ar1.length) console.log(`${ar1} array ${ar2} ga 100% teng`);
+        else console.log('Bu arraylarning faqat uzunliklari teng!');
+    }
+
+}
+// func([1 , 2 , 0 , 4] , [1 , 2 , 3 , 4])
+
+//! 4
+
+let camelize = (str) => {
+  let array = str.split("-") , s = "";
+  array.forEach((e)=> {
+    if(e !== ""){
+        let res = e.replace(e[0] , e[0].toUpperCase())
+        s += res
+    }
+  })
+  console.log(s);
+}
+// camelize('background-color');
+// camelize('list-stye-image');
+// camelize('-webkit-transition');
