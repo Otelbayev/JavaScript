56a85d42cacab167e6187d08b4e15114b63bc26b
Jasurbek <jasurdev1604@gmail.com>
2023-02-27 14:06:36 +0500

    CRUD

--- 10-dars/home.js
+++ 10-dars/home.js
@@ -17,9 +17,14 @@ let countStr = (str) => {
 //stringdagi @ ni a hatfiga o'zgartirish
 
 let replace = (str) => {
-    
+    let res1 = str.split("") , index;
+    for(i in res1){
+        if(res1[i] === '@') index = i;
+    }
+    let res = res1.fill('a' , Number(index) , Number(index) + 1);
+    console.log(res.join(""));
 }
-replace('j@subek')
+// replace('j@subek')
 
 //!3
 //krtma ket kelgan harflarni ochitrish va bittasini qoldirish
@@ -69,15 +74,61 @@ let palindrom = (str) => {
 // palindrom('non')
 
 //!7
-// ikki stringni solishtiring length va harflari bir hilliginu ms: web = bwe
+// ikki stringni solishtiring length va harflari bir xilligini ms: web = bwe
+
+let epamExe = (a , b) => {
+    let x = 0;
+    for(let i = 0; i < a.length; i++){
+        if(a.length === b.length && b.includes(a[i]) && a.includes(b[i])) x++
+        else x--;
+    }
+    x === a.length ? console.log(true) : console.log(false);
+}
+// epamExe('web' , 'ebw')
 //!8
 //stringdagi harflarni nechtadan qatnashganligini chiqaring
+
+let str = (str) => {
+    let obj = {};
+    for(let i of str) {
+        if(i in obj) obj[i]++;
+        else obj[i] = 1
+    }
+    console.log(obj);
+}
+// str('jasurbekk')
+
 //!9
 //stringni ziplash webbrainn = we2brai2n
+
+let zip = (str) => {
+    let count = 1 , s = "";
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === str[i+1]){
+            count++;
+            continue
+        }
+        else {
+            count === 1 ? s += str[i] : s += str[i] + count
+        }
+        count = 1
+    }
+    console.log(s);
+}
+// zip('jjasssurrrrr')
 
 //! Objects
 //!1
 //obj keylaini chiqarign
+let obj0 = {
+    name:'jasurbek',
+    sname: 'otelbayev',
+    age: 18 
+}
+for(i in obj0) {
+    // console.log(i)
+}
+
 //!2
 // agelar yigindisini toping
 
@@ -87,10 +138,17 @@ let obj = {
     child:{
         age:4,
         child:{
-            age:3
+            age:4,
         }
     }
 }
+
+let sum = 0;
+for(key in obj) {
+    sum += obj.age;
+    obj = obj.child
+}
+// console.log(sum);
 
 //!3
 //boolean typedagi malumotlarni chiqwaring keylari bilan birga
@@ -102,11 +160,29 @@ let obj1 = {
     indidual:false
 }
 
+for(i in obj1){
+    if(typeof obj1[i] === 'boolean') {
+        // console.log(i , obj1[i]);
+    }
+}
+
 //! Arrays
 
 //!1
 // dublicatelarni remove qilish
-let num = [2 , 3 , 5 , 2 , 3 , 5 , 4 , 8 , 6 , 3];
+
+let num = [2, 3, 5, 2, 3, 5, 4, 8, 6, 3];
+function removeDuplicates(arr) {
+    let emptyArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(emptyArr.indexOf(arr[i]) === -1){
+            emptyArr.push(arr[i])
+        }
+    }
+    console.log(emptyArr);
+}
+
+// removeDuplicates(num)
 
 //!2
 //taqqoslang
@@ -114,9 +190,22 @@ let num = [2 , 3 , 5 , 2 , 3 , 5 , 4 , 8 , 6 , 3];
 let num1 = [1 , 2 , 3]
 let num2 = [1 , 2 , 3]
 
+let count = 0
+for(i in num1) {
+    if(num.length === num.length && num1[i] == num2[i]) count++
+    else count--;
+}
+// count === num1.length ? console.log('bir biriga teng') : console.log('teng emas');
+
 //!3 
 // arraydagi ikki honali sonlarni chiqaring
-let array = [1 , 23 , 56 , -852];
+let array = [1 , 23 , 56 , -25 , -852];
+
+for(let i of array) {
+    if(i >=10 && i <= 99 || i <= -10 && i >= -99){
+        // console.log(i);
+    }
+}
 
 //!homework
 
@@ -125,3 +214,10 @@ let array = [1 , 23 , 56 , -852];
 // [2 , 2 , 1 , 2]
 // [1 , 2]
 // [1]
+
+
+
+
+
+
+
