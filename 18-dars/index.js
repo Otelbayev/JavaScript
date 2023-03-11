b94b71f915355e6f975fe54a6a5644ebcc8a7419
Jasurbek <jasurdev1604@gmail.com>
2023-03-11 21:27:14 +0500

    DDOS attack

--- 18-dars/index.js
+++ 18-dars/index.js
@@ -1,23 +1,46 @@
 //! DDOS Attack
 
-function getData() {
-    return fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json())
+// function getData() {
+//     return fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json())
+// }
+
+// function getName() {
+//     return fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json())
+// }
+
+// function getCached(func) {
+//     let cache = new Map()
+//     return function(x){
+//         if(cache.has(x)){
+//             return cache.get(x)
+//         } else {
+//             let res = func();
+//             return cache.set(x , res)
+//         }
+//     }
+// }
+
+// getData = getCached(getData)
+
+let obj = {
+    user:'users',
+    getData(){
+        fetch(`https://jsonplaceholder.typicode.com/${this.user}`)
+        .then((res)=>res.json())
+        .then(res => console.log(res))
+    }
 }
 
-function getName() {
-    return fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json())
-}
-
-function getCached(func) {
-    let cache = new Map()
-    return function(x){
-        if(cache.has(x)){
-            return cache.get(x)
+let getCached = (func) => {
+    let map = new Map();
+    return function (x) {
+        if(map.has(x)){
+            console.log(map.get(x));
         } else {
-            let res = func();
-            return cache.set(x , res)
+            let res = func.call(this)
+           return map.set(x , res);
         }
     }
 }
 
-getData = getCached(getData)
+obj.getData = getCached(obj.getData)
