f467b27410eb0feddafab6a7ce173f605acaa8b8
Jasurbek <jasurdev1604@gmail.com>
2023-03-18 11:12:29 +0500

    Generator funtions

--- 24-dars/index.js
+++ 24-dars/index.js
@@ -1,36 +1,105 @@
-//! multiple returnga qatyuvchi funksiya
+// let data = new Promise((resolve , reject) => {
+//     setTimeout(() =>{
+//         resolve('code.')
+//     })
+// })
 
-function* getData() {
-    //!yield
-    yield 1;
-    yield 2;
-    yield 3;
-}
-
-const generatior = getData();
-
-// console.log(generatior.next());
-// console.log(generatior.next());
-// console.log(generatior.next());
-// console.log(generatior.next());
-
-//! loop -> valuelar boyicha yurib chiqadi
-// for (let vl of generatior) {
-//     // console.log(vl);    
+// async function get() {
+//     console.log('start');
+//     let res = await data;
+//     console.log(res);
+//     console.log('finish!');
 // }
 
-//! valuelarni massiv qilib beradi serad operator
-// console.log([...generatior]);
+// get()
 
-const users = [
-    {id:1, name:'eshmat1'},
-    {id:2, name:'eshmat2'},
-    {id:3, name:'eshmat3'},
-    {id:4, name:'eshmat4'},
-    {id:5, name:'eshmat5'},
-    {id:6, name:'eshmat6'},
+//! Generator function
+
+//yield , return
+function* getData() {
+    let a = 0
+    while(1){
+        yield ++a
+        if(a === 3) {
+            return a
+        }
+    }
+}
+//! yeild genfuntionni pauza qiladi
+//! return genfunctionni toxtatadi
+let generator = getData()
+
+
+// console.log(generator.next());//!{value:2 , done:false}
+// console.log(generator.next());//!{value:3 , done:false}
+// console.log(generator.next());//!{value:undefind , done:true}
+
+//!loop
+
+// for(vl of generator){
+    // console.log(vl); //! valuselarnini olip beradi
+// }
+
+//!spread -> array korinishida valuelar
+// console.log([...generator]);
+
+function* get() {
+    let id = users.length
+    while(1){
+        yield ++id
+    }
+}
+let get1 = get()
+
+
+let users = [
+    {id:1 , name:"eshmat"},
+    {id:2 , name:"eshmat"},
+    {id:3 , name:"eshmat"},
+    {id:4 , name:"eshmat"},
+    {id:5 , name:"eshmat"},
+    {id:6 , name:"eshmat"},
 ]
 
-const onDelate = (id) => {
+let onDel = (id) => {
     let res = users.filter((v) => v.id !== id)
+    users = res
 }
+
+let add = (name) => {
+    users = [...users , {id:get1.next().value , name:name}]
+}
+
+add('none')
+onDel(3)
+add('qwert')
+add('poiuytr')
+// console.log(users);
+
+//! parametr
+
+function* getParam(n) {
+    for(vl of n){
+        yield vl;
+        let newValue = yield vl; // .next ichidagi parametr
+    }
+}
+
+const gen = getParam([1 , 2 , 6])
+
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+
+
+function* getNext() {
+    let  i = 0;
+    while(1){
+        let n = yield ++i
+        if(n) i += n
+    }
+}
+const r = getNext()
+console.log(r.next());
+console.log(r.next(3));
+console.log(r.next(4));
