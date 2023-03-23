3adaf75b16c5cb623eff558ba0977cc88059cbf6
Jasurbek <jasurdev1604@gmail.com>
2023-03-23 16:13:10 +0500

    modules

---
+++ 25-dars/index.js
@@ -0,0 +1,12 @@
+export let users = [
+    {id:1, name:'jasurbek' , age:18 },
+    {id:2, name:'jasurbek' , age:18 },
+    {id:3, name:'jasurbek' , age:18 },
+    {id:4, name:'jasurbek' , age:18 },
+    {id:5, name:'jasurbek' , age:18 },
+    {id:6, name:'jasurbek' , age:18 },
+    {id:7, name:'jasurbek' , age:18 },
+    {id:8, name:'jasurbek' , age:18 },
+    {id:9, name:'jasurbek' , age:18 },
+    {id:10, name:'jasurbek' , age:18 },
+]
