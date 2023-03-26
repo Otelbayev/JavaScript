ab0c2c0bfbea8706409f5a28b60aedfc3565b2e0
Jasurbek <jasurdev1604@gmail.com>
2023-03-26 16:19:58 +0500

    Proxy

--- 27-dars/index.js
+++ 27-dars/index.js
@@ -1,40 +1,62 @@
 //! Proxy
 let user = {
     name:'jasurbek',
-    _stir: 123456
+    _stir: 123456,
+    start :1,
+    end:10
 }
-//! get 
+
 let proxy = new Proxy(user , {
+    //! get 
     get(target , prop){
         // console.log(target);
         if(prop.startsWith("_")) return `acces for ${prop} is dineid`
         else return target[prop]
+    },
+    //! set
+    set(target , prop , value){
+        if(prop.startsWith("_")) return false
+        else {
+            target[prop] = value
+            return true
+        }
+    },
+    //!ownKeys
+    ownKeys(targert){
+        return Object.keys(targert).filter((v) => !v.startsWith('_'))
+    },
+    //!has
+    has(targert , prop) {
+        return targert.start <= prop && prop <= targert.end
     }
 })
 
+// console.log(2 in proxy);//!has
+
+
+// console.log(proxy);
+// for(let i in proxy) {
+//     console.log(i); //! ownKeys
+// }
+
+// console.log(Object.keys(proxy))
+// console.log(Object.values(proxy))
+// console.log(Object.entries(proxy))
+
+// proxy._stir = 78
+// console.log(proxy);
 //! targert => {name : 'jasurbek' , _stir: 123456}
 //! prop => name  or _stir
 // console.log(user);
 // console.log(proxy._stir);
 
-//! set
 
-proxy.age = 18
+// proxy.age = 18
 // console.log(proxy);
 
-let user1 ={
-    username:'jasur',
-    password:'Jasurbek2004'
-}
+//! Reflect 
 
-let proxy1 = new Proxy(user1 , {
-    set(targert , prop , value){
-        if(prop = 'password') return false
-        targert[prop] = value
-        return true
-    }
-})
 
-proxy1.none = "none"
-
-console.log(proxy1)
+Reflect.set(user , "qwert" , "qwerty")
+console.log(Reflect.get(user , 'name'));
+console.log(user);
