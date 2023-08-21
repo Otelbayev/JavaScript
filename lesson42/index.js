384f49c27beb9df202c49164c11f18e5dc8df18e
jasurbek <jasurdev1604@gmail.com>
2023-08-21 05:39:45 +0500

    Clickjank attack

---
+++ lesson42/index.js
@@ -0,0 +1,9 @@
+//! 42 - dars | Clickjank attack | YouTube Link
+// 	—  clickjank attack qachon bo'ladi?
+// 	—  Same Origin Policy
+// 	—  Blocking top navigation
+// 	—  X-Frame-Option
+
+const getData = (e) => {
+  console.log("your accaunt is hacked");
+};
