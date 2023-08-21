85529b2675565abec76a92d67c862ae309cb568e
jasurbek <jasurdev1604@gmail.com>
2023-08-22 00:56:40 +0500

    Fetch Basic Advencrd

---
+++ lesson47/index.js
@@ -0,0 +1,24 @@
+//! 47 - dars | Fetch - 3 | Advanced | YouTube Link
+// 	—  Sending Image
+// 	—  Swagger in detail.
+// 	—  Multipart file
+// 	—  Form Data
+// 	—  ReadbleStream
+// 	—  Fetch API's
+
+// const login = () => {
+//   fetch("http://localhost:8080/api/public/auth/login", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       email: email.value,
+//       password: password.value,
+//     }),
+//   })
+//     .then((res) => res.json())
+//     .then((res) => {
+//       console.log(res);
+//     });
+// };
