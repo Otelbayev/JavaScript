947e4e5df95b630e984c03a4a7189a0e816ca7be
jasurbek <jasurdev1604@gmail.com>
2023-08-30 23:47:18 +0500

    Fetch Advenced

--- lesson47/index.js
+++ lesson47/index.js
@@ -6,21 +6,34 @@
 // 	—  ReadbleStream
 // 	—  Fetch API's
 
-const login = () => {
+// const login = () => {
+//   fetch("http://localhost:8080/api/public/auth/login", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       email: email.value,
+//       password: password.value,
+//     }),
+//   })
+//     .then((res) => res.json())
+//     .then((res) => console.log(res));
+// };
+
+fetch("./txt.txt")
+  .then((res) => res.text())
+  .then((res) => console.log(res));
+
+formEvent.onsubmit = (e) => {
+  e.preventDefault();
   fetch("http://localhost:8080/api/public/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      email: email.value,
-      password: password.value,
-    }),
+    body: new FormData(formEvent),
   })
     .then((res) => res.json())
     .then((res) => console.log(res));
 };
-
-fetch("./txt.txt")
-  .then((res) => res.text())
-  .then((res) => console.log(res));
