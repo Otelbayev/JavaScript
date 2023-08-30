15f407f3e42cf5383f7e4449f570b120ec430396
jasurbek <jasurdev1604@gmail.com>
2023-08-30 23:22:07 +0500

    Fetch Advenced

--- lesson47/index.js
+++ lesson47/index.js
@@ -6,40 +6,16 @@
 // 	—  ReadbleStream
 // 	—  Fetch API's
 
-// const login = () => {
-//   fetch("http://localhost:8080/api/public/auth/login", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       email: email.value,
-//       password: password.value,
-//     }),
-//   })
-//     .then((res) => res.json())
-//     .then((res) => {
-//       console.log(res);
-//     });
-// };
-
-// fetch("./txt.txt")
-//   .then((res) => res.text())
-//   .then((res) => console.log(res));
-
-//!Form Data
-
-formEvent.onsubmit = (e) => {
-  e.preventDefault();
-  let body = new FormData(formEvent);
-  //   let imgUrl = URL.createObjectURL(file.files[0]);
-  //   console.log(imgUrl);
-  body.append("userImg", file.files[0]);
-  body.append("roleIdSet", "1");
-  body.delete("password");
-  fetch("http://httpbin.org/post", {
+const login = () => {
+  fetch("http://localhost:8080/api/public/auth/login", {
     method: "POST",
-    body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
   })
     .then((res) => res.json())
     .then((res) => console.log(res));
