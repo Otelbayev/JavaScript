2afa2a9937e40d0e0bef7d380a3379d1dc926219
jasurbek <jasurdev1604@gmail.com>
2023-08-31 23:27:53 +0500

    Fetch Advenced

--- lesson47/index.js
+++ lesson47/index.js
@@ -46,5 +46,7 @@ formEvent.onsubmit = (e) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email: email.value, password: password.value }),
-  }).then((res) => res.json());
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 };
