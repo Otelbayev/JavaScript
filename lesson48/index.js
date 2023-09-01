db4ee80a2b0362e8232805560501df1eb1aa942d
jasurbek <jasurdev1604@gmail.com>
2023-09-01 22:34:10 +0500

    URL Search Params

---
+++ lesson48/index.js
@@ -0,0 +1,6 @@
+//! 48- dars | URL | YouTube Link
+// 	—  new URL
+// 	—  protocol / pathname / host / href / search / hash
+// 	—  SearchParams
+// 	—  URL bo'yicha filter qilish.
+// 	—  URL bo'yicha share qilish
