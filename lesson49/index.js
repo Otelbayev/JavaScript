4cc34aae775c4159771efeda70d0386f2697adfc
jasurbek <jasurdev1604@gmail.com>
2023-09-02 23:35:43 +0500

    Websocked

---
+++ lesson49/index.js
@@ -0,0 +1,9 @@
+//! 49 - dars | Websocket | Backend ExpressJS | YouTube Link
+// 	—  new Websocket
+// 	—  open / message /error / close
+// 	—  ws:// and wss://
+// 	—  SearchParams
+// 	—  Websocket Protocol
+// 	—  Data Transfer
+// 	—  Chatting application
+// 	—  Backend with expressJS
