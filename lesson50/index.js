d24bbf022c3a4fea3022ce2bdea38cc8c6eaa083
jasurbek <jasurdev1604@gmail.com>
2023-09-02 23:37:07 +0500

    LocalStorage 1

---
+++ lesson50/index.js
@@ -0,0 +1,5 @@
+//! 50 - dars | LocalStorage | YouTube Link
+// 	— LocaleStorage nima va qay vaziyatlarda ishlatiladi?
+// 	— Malumotlarni LocalStorage'dan olish / unga qo'shish
+// 	—  Malumotlar o'chirish va clear qilish
+// 	—  LocalStorageni loop qilish
