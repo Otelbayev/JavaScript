e47f544254b8c760be69537e883cddb7cf332a9e
jasurbek <jasurdev1604@gmail.com>
2023-09-03 23:55:39 +0500

    LocalStorage 2

---
+++ lesson51/index.js
@@ -0,0 +1,5 @@
+//! 51 - dars | SessionStorage | YouTube Link
+// 	— LocaleStorage nima va qay vaziyatlarda ishlatiladi?
+// 	— Malumotlarni SessionStorage'dan olish / unga qo'shish
+// 	—  Malumotlar o'chirish va clear qilish
+// 	—  SessionStorage'ni loop qilish
