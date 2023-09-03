3d41827f803dd05df0c924d792d333f63044e235
jasurbek <jasurdev1604@gmail.com>
2023-09-03 23:56:33 +0500

    Cookie

---
+++ lesson52/index.js
@@ -0,0 +1,10 @@
+//! 52 - dars | Cookies | YouTube  Link
+// 	—  what is cookies?
+// 	—  expires or max-age?
+// 	—  session cookies?
+// 	—  delete Cookie
+// 	—  XSRF attack nima?
+// 	—  Pul o'tkazmalarini qanday hack qilsa bo'ladi, agar...?
+// 	—  Cookies flaglari haqida.
+// 	—  Secure
+// 	—  samesite -> lax va strict
