aabb75a3feff9b8303cafedcdd85a83b6fddedd5
jasurbek <jasurdev1604@gmail.com>
2023-09-04 22:57:40 +0500

    indexedDB - 2]

---
+++ lesson55/index.js
@@ -0,0 +1,8 @@
+//! 55 - dars | indexedDB - 2 | Offline applications | YouTube  Link
+// 	—  Index.
+// 	—  Malumot qo'shishdagi options.
+// 	—  Filter by data value / name / grade
+// 	—  IDBKeyRange
+// 	—  LowerBound / UperBound orqali filter qilish
+// 	—  bound orqali filter qilish.
+// 	—  "A", "Z" va "a", "z" bo'yicha filterlash.
