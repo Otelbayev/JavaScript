587acb653ecb02cee47d46bb51980ef895ea1507
jasurbek <jasurdev1604@gmail.com>
2023-09-04 22:56:05 +0500

    indexedDB - 1

---
+++ lesson54/index.js
@@ -0,0 +1,12 @@
+//! 54 - dars | indexedDB - 1 | Offline applications | YouTube Link
+// 	—  indexedDB nima va qachon ishlatiladi?
+// 	—  offline database ochish.
+// 	—  Request events haqida.
+// 	—  DB versiyasini yangilash.
+// 	—  ObjectStore yasash haqida.
+// 	—  Transactions'lar nima?
+// 	—  Bankdan pul o'tkazmalarni indexedDB orqali amalaga oshirish tartiblari.
+// 	—  C reate DBStore.
+// 	—  R ead DBStore.
+// 	—  U pdate DBStore.
+// 	—  D elete DBStore.
