b01ea13dbc3513066af31e235b659621662df66e
jasurbek <jasurdev1604@gmail.com>
2023-09-05 15:21:37 +0500

    Fetch Advenced

--- lesson47/index.js
+++ lesson47/index.js
@@ -27,16 +27,20 @@
 
 // formEvent.onsubmit = (e) => {
 //   e.preventDefault();
-//   fetch("http://localhost:8080/api/public/auth/login", {
+//   let form = new FormData(formEvent);
+
+//   form.append("userImg", file.files[0]);
+//   form.append("roleIdSet", "1");
+
+//   fetch("https://httpbin.org/post", {
 //     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: new FormData(formEvent),
+//     body: form,
 //   })
 //     .then((res) => res.json())
 //     .then((res) => console.log(res));
 // };
+
+let token = null;
 
 formEvent.onsubmit = (e) => {
   e.preventDefault();
@@ -45,7 +49,36 @@ formEvent.onsubmit = (e) => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: new FormData(formEvent),
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      token = res?.authenticationToken;
+      localStorage.setItem("token", token);
+    });
+};
+
+const getCategory = () => {
+  fetch("http://localhost:8080/api/v1/categories/list")
+    .then((res) => res.json())
+    .then((res) => {
+      res.data.map((e) => {
+        console.log(e.name, e.id);
+        let div = document.createElement("div");
+        div.innerHTML = `<h1>${e.name} - ${e.id} <button onclick="getSingle()">selected</button> </h1>`;
+        btn.after(div);
+      });
+    });
+};
+
+const getSingle = () => {
+  fetch("http://localhost:8080/api/v1/categories/1", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   })
     .then((res) => res.json())
     .then((res) => console.log(res));
