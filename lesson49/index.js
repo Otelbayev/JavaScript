d8edbf8507582121205744ed6212c1d749d6546a
jasurbek <jasurdev1604@gmail.com>
2023-09-06 02:48:17 +0500

    Websocked

--- lesson49/index.js
+++ lesson49/index.js
@@ -8,32 +8,32 @@
 // 	—  Chatting application
 // 	—  Backend with expressJS
 
-// let socket = new WebSocket("ws://127.0.0.1:5500/lesson49/");
+let socket = new WebSocket("ws://127.0.0.1:5500/lesson49/");
 
-// socket.onopen = () => {
-//   console.log("connected!");
-// };
+socket.onopen = () => {
+  console.log("connected!");
+};
 
-// socket.onmessage = (event) => {
-//   let div = document.createElement("div");
-//   div.innerHTML = event?.data;
-//   console.log(event);
-//   msg.append(div);
-// };
+socket.onmessage = (event) => {
+  let div = document.createElement("div");
+  div.innerHTML = event?.data;
+  console.log(event);
+  msg.append(div);
+};
 
-// send.onclick = () => {
-//   socket.send(value.value);
-//   value.value = "";
-// };
+send.onclick = () => {
+  socket.send(value.value);
+  value.value = "";
+};
 
-// socket.onerror = () => {
-//   console.log(error);
-// };
+socket.onerror = () => {
+  console.log(error);
+};
 
-// socket.onclose = () => {
-//   console.log("close");
-// };
+socket.onclose = () => {
+  console.log("close");
+};
 
-fetch("http://localhost:8080/users")
-  .then((res) => res.json())
-  .then((res) => console.log(res));
+// fetch("http://localhost:8080/users")
+//   .then((res) => res.json())
+//   .then((res) => console.log(res));
--- lesson49/server.js
+++ lesson49/server.js
@@ -1,38 +1,38 @@
-// const WebSocket = require("ws");
+const WebSocket = require("ws");
 
-// let ws = new WebSocket.Server({ port: 5500 });
+let ws = new WebSocket.Server({ port: 5500 });
 
-// ws.on("connection", (e) => {
-//   console.log("New client connected!");
+ws.on("connection", (w) => {
+  console.log("New client connected!");
 
-//   ws.on("close", (e) => {
-//     console.log("New client disconnected!");
-//   });
+  w.on("close", (e) => {
+    console.log("New client disconnected!");
+  });
 
-//   ws.onmessage = (e) => {
-//     ws.clients.forEach((client) => {
-//       client.send(e.data);
-//     });
-//   };
+  w.onmessage = (e) => {
+    ws.clients.forEach((client) => {
+      client.send(e.data);
+    });
+  };
+});
+
+// const express = require("express");
+// const cors = require("cors");
+
+// let app = express();
+
+// app.use(cors());
+
+// app.use("/users", (req, res) => {
+//   res.send([
+//     {
+//       id: 1,
+//       name: "jasurbek",
+//       age: 19,
+//     },
+//   ]);
 // });
 
-const express = require("express");
-const cors = require("cors");
-
-let app = express();
-
-app.use(cors());
-
-app.use("/users", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "jasurbek",
-      age: 19,
-    },
-  ]);
-});
-
-app.listen(8080, () => {
-  console.log("server running on 8080");
-});
+// app.listen(8080, () => {
+//   console.log("server running on 8080");
+// });
