524fa004dfca9701d91fac7f6cd9d35590cfcd50
jasurbek <jasurdev1604@gmail.com>
2023-09-06 02:44:28 +0500

    Websocked

--- lesson49/index.js
+++ lesson49/index.js
@@ -7,3 +7,33 @@
 // 	—  Data Transfer
 // 	—  Chatting application
 // 	—  Backend with expressJS
+
+// let socket = new WebSocket("ws://127.0.0.1:5500/lesson49/");
+
+// socket.onopen = () => {
+//   console.log("connected!");
+// };
+
+// socket.onmessage = (event) => {
+//   let div = document.createElement("div");
+//   div.innerHTML = event?.data;
+//   console.log(event);
+//   msg.append(div);
+// };
+
+// send.onclick = () => {
+//   socket.send(value.value);
+//   value.value = "";
+// };
+
+// socket.onerror = () => {
+//   console.log(error);
+// };
+
+// socket.onclose = () => {
+//   console.log("close");
+// };
+
+fetch("http://localhost:8080/users")
+  .then((res) => res.json())
+  .then((res) => console.log(res));
