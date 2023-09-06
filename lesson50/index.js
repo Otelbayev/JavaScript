675ab7a5c59e6cc695fa1fea6fcd602285519497
jasurbek <jasurdev1604@gmail.com>
2023-09-06 15:04:21 +0500

    LocalStorage 1

--- lesson50/index.js
+++ lesson50/index.js
@@ -3,3 +3,35 @@
 // 	— Malumotlarni LocalStorage'dan olish / unga qo'shish
 // 	—  Malumotlar o'chirish va clear qilish
 // 	—  LocalStorageni loop qilish
+
+if (localStorage.getItem("count")) {
+  count.innerText = localStorage.getItem("count");
+} else {
+  count.innerText = 0;
+}
+
+const plus = () => {
+  count.innerText = eval(`${count.innerText} + 1`);
+  localStorage.setItem("count", count.innerText);
+};
+
+const minus = () => {
+  count.innerText = eval(`${count.innerText} - 1`);
+  localStorage.setItem("count", count.innerText);
+};
+
+const deleteL = () => {
+  localStorage.removeItem("count");
+  count.innerText = 0;
+};
+
+const clearing = () => {
+  localStorage.clear();
+  count.innerText = 0;
+  title.innerText = "";
+};
+
+const onChange = ({ target: { value } }) => {
+  localStorage.setItem("title", value);
+  title.innerText = value;
+};
