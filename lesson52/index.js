3e854ccdeb557290b5aaf4d4079250f5a24e1347
jasurbek <jasurdev1604@gmail.com>
2023-09-07 10:11:40 +0500

    Cookies

--- lesson52/index.js
+++ lesson52/index.js
@@ -8,3 +8,14 @@
 // 	—  Cookies flaglari haqida.
 // 	—  Secure
 // 	—  samesite -> lax va strict
+
+let date = new Date(2023, 7, 9);
+date = date.toUTCString();
+console.log(date);
+document.cookie = `name=jasurbek; expire=${date};`;
+
+document.cookie = "title = none;max-age=1;secure";
+//! secure -> qabul qininaytgan maumot boshqa saytdan qabul qilinmaydi
+//! sameside=lax; -> faqat cookiesdagi malumotni korish munkin uni GET,PUT, DELETE, qilish ni imkoni yoq
+//! sameside=strict; -> boshqa side dan malumot olib kelishiini oldini oladi
+// console.log(document.cookie);
