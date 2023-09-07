4001001db13c7770fcb95d156e089d30095a65b7
jasurbek <jasurdev1604@gmail.com>
2023-09-07 10:39:52 +0500

    Encoding and Decoding

--- lesson53/index.js
+++ lesson53/index.js
@@ -4,3 +4,20 @@
 // 	—  encodeURIComponent
 // 	—  new URL.
 // 	—  searchParams with encode and decoders
+
+let city = "sebzor 3a";
+let url = new URL(`http://127.0.0.1:5501/lesson53/?name=jasurbek&age=19&`);
+
+let search = url.searchParams;
+
+search.set("city", city);
+search.set("address", encodeURI(city)); //!-> ignore !,#,$,%~<~^
+search.set("home", encodeURIComponent(city));
+
+console.log(url);
+
+//!new URL -> avtomatic tarda encode ecode qilib beradi
+
+console.log(decodeURI(search.get("address")));
+console.log(decodeURIComponent(search.get("home")));
+console.log(search.get("city"));
