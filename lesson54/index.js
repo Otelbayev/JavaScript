67e2f4a3df359c6b825cabd86415c9ffcacbb020
jasurbek <jasurdev1604@gmail.com>
2023-09-07 12:14:44 +0500

    indexedDB - 1

--- lesson54/index.js
+++ lesson54/index.js
@@ -10,3 +10,32 @@
 // 	—  R ead DBStore.
 // 	—  U pdate DBStore.
 // 	—  D elete DBStore.
+
+// let request = window.indexedDB.open(databaseName , version);
+let request = window.indexedDB.open("jasurbek", 2);
+
+var db = null;
+
+request.onsuccess = (e) => {
+  //   db = e.target.result;
+  db = request.result;
+  console.log("success");
+};
+
+request.onerror = () => {
+  console.log("error");
+};
+
+request.onupgradeneeded = () => {
+  console.log("onupgradeneeded");
+  db = request.result;
+  if (!db.objectStoreNames.contains("frontend")) {
+    let store = db.createObjectStore("frontend", { keyPath: "id" });
+    store.onsuccess = (e) => {
+      console.log("succesfully store created");
+    };
+    store.onerror = (e) => {
+      console.log("store error");
+    };
+  }
+};
